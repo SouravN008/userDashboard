cd1a6d45b5fc926af15065d63499e8646f579262
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-11-04 11:25:49 +0530

    fake user data added and link with react and userDispatch hook

--- src/Store/slice/UserSlice.jsx
+++ src/Store/slice/UserSlice.jsx
@@ -3,10 +3,14 @@ const userSlice = createSlice({
    name : "user",
    initialState : [],
    reducers : {
-       addUser(state, action) {},
+       addUser(state, action) {
+        state.push(action.payload);
+        console.log(action.payload)
+       },
        removeUser(state, action){},
        deleteUser(state , action) {} ,
    },
 });
 console.log(userSlice.actions);
 export default userSlice.reducer;
+export const {addUser} = userSlice.actions;
